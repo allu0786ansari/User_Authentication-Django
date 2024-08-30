f02df575fe13e71c03eb08a8982727c137f3cc41
allu0786ansari <allu456654ansari@gmail.com>
2024-08-31 00:42:13 +0530

    updated

--- DemoProject/settings.py
+++ DemoProject/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'DemoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'finance_assist',
-        'USER': 'root',
-        'PASSWORD': 'A6VLT@$cuDYA.4n',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
